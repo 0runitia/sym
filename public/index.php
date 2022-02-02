762a563a998cc7d2ef1c81ad1a33fe036ea8ceff
0runitia <valcommerot@gmail.com>
2022-02-02 17:07:32 +0100

    formulé

--- public/index.php
+++ public/index.php
@@ -1,9 +1,12 @@
-<?php
-
-use App\Kernel;
-
-require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
-
-return function (array $context) {
-    return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
-};
+<form method="post" action="traitement.php">
+    <p>
+        <label for="login">Votre Login :</label>
+        <input type="text" name="login" id="login" placeholder="Login" size="30" maxlength="10" required/>
+<br>
+<br>
+        <label for="pass">Votre mot de passe :</label>
+        <input type="password" name="pass" id="pass" required/>
+    </p>
+    <br>
+<input type="submit" value="Envoyer" />
+</form>
